14bc67286db9bf22dbae642f756251e35518d7fd
Wiktoria Szramkowska <151945560+wszramkowska@users.noreply.github.com>
2025-10-22 16:46:28 +0100

    doxygen code example

---
+++ src/example.cpp
@@ -0,0 +1,10 @@
+/// @file example.cpp
+/// @brief Simple function example for Doxygen.
+
+/// Adds two integers together.
+/// @param a First number
+/// @param b Second number
+/// @return Sum of a and b
+int add(int a, int b) {
+    return a + b;
+}
